601b204e982813c2fa028c33fcdd19322c3b22d7
Sagan <61011062@kmitl.ac.th>
2020-11-25 03:31:29 +0700

    fntest01

--- inventories/settings.py
+++ inventories/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'inventories.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fnproject01',
+        'USER': 'postgres',
+        'PASSWORD': 'kongbeng2000',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
--- stock/urls.py
+++ stock/urls.py
@@ -2,8 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
-    path('about', views.about, name = 'about'),
-    path('contact', views.contact, name = 'contact')
-
+    path('', views.index, name='index'),
+    path('games', views.games, name='games'),
+    path('about-fngame', views.about, name='about-fngame'),
+    path('login', views.login, name='login'),
+    path('signup', views.signup, name='signup'),
+    path('myaccount', views.account, name='myaccount'),
+    path('adduser', views.adduser, name='adduser'),
+    path('userin', views.userin, name='userin'),
+    path('userout', views.userout, name='userout'),
+    path('change_password', views.change_password, name='change_password'),
 ]
